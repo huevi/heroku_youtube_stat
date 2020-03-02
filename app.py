de572379699f4a38cd1335ed8eb11fcf2066ec40
hari <hari03@live.in>
2020-03-03 00:29:11 +0530

    scraper
    q
    exit

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 
+import os
+
 def main():
     """"simple app"""
     st.title("A simple app")
+    title = st.text_input('Enter password', 'password')
+    if title == "Newtime":
+        scrape = True
+    if st.button('scrape youtube trends'):
+        st.write('Why hello there')
+        if scrape:
+            os.system("python scraper.py")
+        else:
+            st.write('enter correct password')
+    else:
+        st.write('Goodbye')
 
 if __name__ == "__main__":
     main()
